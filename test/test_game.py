a1f9915ce6c0158687c00b7ded1e5316cf159e6e
MehdiZouitine <39647484+MehdiZouitine@users.noreply.github.com>
2020-09-26 13:16:21 +0200

    Create test_game.py

---
+++ test/test_game.py
@@ -0,0 +1,35 @@
+from gym_ma_toy.envs.game import World
+import numpy as np
+
+
+class TestGame:
+    def test_attribute():
+        size_list = [10, 20, 100, 200, 1000, 2000, 3000]
+        nb_agents_list = [2, 4, 10, 20, 100, 200, 1000]
+        nb_targets_list = [2, 4, 10, 20, 100, 200, 1000]
+        worlds = [World(size=size_list[i], nb_agents=nb_agents_list[i],
+                        nb_targets=nb_targets_list[i], seed=7) for i in range(len(size_list))]
+        for i in range(len(size_list)):
+            assert ((worlds[i].size == size_list[i]) and
+                    (worlds[i].nb_agents == nb_agents_list[i]) and
+                    (worlds[i].nb_targets == nb_targets_list[i]) and
+                    (worlds[i].seed == 7))
+
+    def test_reset():
+        size_list = [10, 20, 100, 200, 1000, 2000, 3000]
+        nb_agents_list = [2, 4, 10, 20, 100, 200, 1000]
+        nb_targets_list = [2, 4, 10, 20, 100, 200, 1000]
+        worlds = [World(size=size_list[i], nb_agents=nb_agents_list[i],
+                        nb_targets=nb_targets_list[i], seed=7) for i in range(len(size_list))]
+        for i in range(len(worlds)):
+            worlds[i].reset()
+            assert ((worlds[i].size == size_list[i]) and
+                    (worlds[i].nb_agents == nb_agents_list[i]) and
+                    (worlds[i].nb_targets == nb_targets_list[i]) and
+                    (worlds[i].seed == 7) and
+                    # check if the border are equal to 0
+                    (np.sum(worlds[i].map[0, :]) == 0) and
+                    (np.sum(worlds[i].map[:, 0]) == 0) and
+                    (np.sum(worlds[i].map[worlds[i].size - 1, :]) == 0) and
+                    (np.sum(worlds[i].map[:, worlds[i].size - 1]) == 0) and
+                    (np.sum(worlds[i].map) >= worlds[i].nb_agents))

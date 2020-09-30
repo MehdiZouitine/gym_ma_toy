b9347cf801d0a03b1d51755512ab6a28679a9624
MehdiZouitine <39647484+MehdiZouitine@users.noreply.github.com>
2020-09-30 20:30:39 +0200

    Update game.py

--- gym_ma_toy/envs/coop/game.py
+++ gym_ma_toy/envs/coop/game.py
@@ -1,8 +1,14 @@
 import abc
-from typing import Tuple
 import numpy as np
+import typing
+from typing import Tuple, Dict
 
-ACTIONS = {"NOOP": 0, "UP": 1, "DOWN": 2, "LEFT": 3, "RIGHT": 4}
+
+TypeAction = Dict[str, int]
+
+
+ACTIONS = {"NOOP": 0, "UP": 1, "DOWN": 2,
+    "LEFT": 3, "RIGHT": 4}  # actions meaning
 
 
 class BaseElem(abc.ABC):
@@ -22,7 +28,31 @@ class Target(BaseElem):
 
 
 class World:
-    def __init__(self, size, nb_agents, nb_targets, seed):
+
+
+<< << << < HEAD
+
+     """
+    Implementation of the team catcher game. The interface will collect observations from that game.
+    """
+    def __init__(self, size:int, nb_agents:int, nb_targets:int, seed:int):
+        """
+
+        Parameters
+        ----------
+        size : int
+            Size of the grid (also called map).
+        nb_agents : int
+            Number of agents.
+        nb_targets : int
+            Number of targets.
+        seed : int
+            Random seed.
+
+        """
+=======
+    def __init__(self, size: int, nb_agents: int, nb_targets: int, seed: int):
+>>>>>>> f709245b03f738d130303d316855d97a697a5be9
         self.seed = seed
         self.size = size
         self.nb_agents = nb_agents
@@ -30,27 +60,40 @@ class World:
         self.targets_alive = self.nb_targets
 
     @property
-    def nb_targets_alive(self):
+    def nb_targets_alive(self) -> int:
+        """Short summary.
+
+        Returns
+        -------
+        int
+            Targets alive.
+
+        """
         return self.targets_alive
 
     def reset(self):
-        self.targets_alive = self.nb_targets
-        self.map = np.zeros((self.size, self.size))
+        # Restart the game
+        self.targets_alive = self.nb_targets # At each episode the targets are resuscitated.
+        self.map = np.zeros((self.size, self.size)) # and the map is cleaned
+
+        '''We want to place 3 targets and 3 targets randomly on the private grid of its border.
+         For that we generate the set of couple (i,j) of our private border map.
+         Then we randomly select nb_agents element in this list and then nb_targets. '''
+
         all_positions = [
             (i, j) for j in range(1, self.size - 1) for i in range(1, self.size - 1)
         ]
-
         random_idx = np.arange(0, (self.size - 2) * (self.size - 2))
         np.random.shuffle(random_idx)
 
-        agents_pos = [all_positions[random_idx[i]]
-                      for i in range(self.nb_agents)]
+        agents_pos = [all_positions[random_idx[i]] for i in range(self.nb_agents)]
+        # first nb_agents are randomly selected
 
         targets_pos = [
             all_positions[random_idx[i]]
             for i in range(self.nb_agents, self.nb_agents + self.nb_targets)
         ]
-
+        # same for targets
         self.agents = {
             f"agent_{i+1}": Agent(id_elem=(i + 1), position=agents_pos[i])
             for (i, pos) in enumerate(agents_pos)
@@ -59,9 +102,9 @@ class World:
         self.targets = [Target(position=pos) for pos in targets_pos]
 
         for pos in agents_pos:
-            self.map[pos[0], pos[1]] = 1
+            self.map[pos[0], pos[1]] = 1 #Agents are represented by 1
         for pos in targets_pos:
-            self.map[pos[0], pos[1]] = 2
+            self.map[pos[0], pos[1]] = 2 # Targets by 2
 
         self._update_map()
         self._update_position_state()
@@ -71,12 +114,22 @@ class World:
         return self.state
 
     def _update_position_state(self):
+        # Updates the current state of the game (which will be returned by the step and reset method of the gym interface)
         self.agent_position = {k: v.position for k, v in self.agents.items()}
         self.state = {"map": self.map, "agent_position": self.agent_position}
 
-    def _update_action(self, action, agent_id):
-        # if action == ACTIONS['NOOP']:
-        #     return True
+    def _update_action(self, action:int, agent_id:str):
+        """
+
+        Parameters
+        ----------
+        action : int
+            Action in {0,1,2,3,4}.
+        agent_id : str
+            Name of the agent : "agent_1" ..., "agent_n"
+
+        """
+        # We check if an action is feasible and do it.
         agent = self.agents[agent_id]
         if action == ACTIONS["UP"]:
             if (
@@ -115,7 +168,24 @@ class World:
                 agent.position = (agent.position[0], agent.position[1] + 1)
 
     @classmethod
-    def agent_capture(cls, pixel, size, map):
+    def agent_capture(cls, pixel:Tuple[int,int], size:int, map:np.ndarray)-> bool:
+        """ Check if a target is captured by the agents
+
+        Parameters
+        ----------
+        pixel : Tuple[int,int]
+            Target location.
+        size : int
+            Size of the map.
+        map : np.ndarray
+            The map.
+
+        Returns
+        -------
+        bool
+            True if an targets is captured, else False.
+
+        """
         potential_neighborhood = [
             (pixel[0] + 1, pixel[1]),
             (pixel[0] - 1, pixel[1]),
@@ -136,6 +206,8 @@ class World:
         return n_agent_neighbour >= 2
 
     def _update_map(self):
+        ''' For each target, we check if it is captured and if so,
+         we delete it. So we reduce the number of live targets by 1.'''
         size = self.size
         for i in range(size):
             for j in range(size):
@@ -144,7 +216,14 @@ class World:
                         self.map[i, j] = 0
                         self.targets_alive -= 1
 
-    def update(self, joint_action):
+    def update(self, joint_action:TypeAction):
+        """Update the map and the agents and targets state
+
+        Parameters
+        ----------
+        joint_action : TypeAction
+            Dict of actions for each agent : {"agent_1" : 0, "agent_2" : 3, ..., "agent_n": 1}
+        """
 
         for agent_id, action in joint_action.items():
             self._update_action(action=action, agent_id=agent_id)

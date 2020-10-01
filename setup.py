c5b14369cf0d77180b8779e1499ee9c79e126218
aib <berjaoui>
2020-10-01 14:49:14 +0200

    Added missing dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name="gym_ma_toy",
     version="0.0.1",
-    install_requires=["gym", "numpy"],  # And any other dependencies required
+    install_requires=["gym", "numpy", "matplotlib", "PIL"],  # And any other
+    # dependencies required
 )

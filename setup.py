724ef64b61e7d2785f5f644cdcfa5b836d8262a5
aib <berjaoui>
2020-10-27 18:06:22 +0100

    Added aenum

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="gym_ma_toy",
     version="0.0.1",
-    install_requires=["gym", "numpy", "matplotlib", "PILLOW"],  # And any other
+    install_requires=["gym", "numpy", "matplotlib", "PILLOW", "aenum"],
+    # And any other
     # dependencies required
 )

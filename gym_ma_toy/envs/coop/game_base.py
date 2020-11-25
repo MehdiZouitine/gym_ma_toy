18f054601fbd5e2e545f2d656b41396edbb31d94
MehdiZouitine <39647484+MehdiZouitine@users.noreply.github.com>
2020-11-25 23:27:10 +0100

    Update game_base.py

--- gym_ma_toy/envs/coop/game_base.py
+++ gym_ma_toy/envs/coop/game_base.py
@@ -344,6 +344,8 @@ class WorldBase:
 
     @property
     def state(self) -> dict:
+        if self.partially_observable:
+            return self._create_fow_state(self.state)
         return {"map": self.map, "agent_position": self.agent_position}
 
     def _update_position_state(self):
